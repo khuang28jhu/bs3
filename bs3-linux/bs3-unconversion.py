d2b02bb81739ef48604dab7e6c438b334801cb2b
khuang28jhu <khuang28@jhu.edu>
2016-10-23 20:23:40 -0400

    Update bs3-unconversion.py

--- bs3-linux/bs3-unconversion.py
+++ bs3-linux/bs3-unconversion.py
@@ -6,14 +6,15 @@ parser = OptionParser()
 parser.add_option('-f', action="store", dest="input", type="string", help="Please supply input file path")
 parser.add_option('-g', action="store", dest="genome", type="string", help="Please supply genome file path")
 options, args = parser.parse_args()
-        
 
-align_cmd = 'python bs3-align.py -i ' + options.input  + ' -o lamda_unconversion -g ' +   options.genome
-call_methyl_cmd = 'python bs3-call_methylation.py -i lamda_unconversion -o lamda_unconversion  --db bs_align/bs_utils/reference_genomes/lamdba.fa_snap/'
+
+align_cmd = 'python bs3-align.py -i ' + options.input  + '  -o lamdba_unconversion -g ' +   options.genome
+call_methyl_cmd = 'python bs3-call_methylation.py -i lamdba_unconversion -o lamda_unconversion  --db bs_align/bs_utils/reference_genomes/lamdba.fa_snap/'
 print_graph_cmd =  'python bs3-methyl_display.py -u y -m lamda_unconversion.CGmap.gz'
 
 subprocess.call('./bs3-build -f test_data/lamdba.fa --aligner=snap', shell=True)
 print 'Aligning the Reads to the Lamda Phage Genome'
+print align_cmd
 subprocess.call(align_cmd, shell = True)
 print 'Generating the Single Base Resolution CG Levl File'
 subprocess.call(call_methyl_cmd, shell = True)
